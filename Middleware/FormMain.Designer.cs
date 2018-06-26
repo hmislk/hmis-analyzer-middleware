a02bae82d88f38eec7ce7549934f099ab82282e3
Buddhika Ariyaratne <buddhika.ari@gmail.com>
2018-06-27 00:54:15 +0530

    Changed Interface Signed-off-by: Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- Middleware/FormMain.Designer.cs
+++ Middleware/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Middleware
 {
-    partial class FormMain
+    partial class FormDimensionSettings
     {
         /// <summary>
         /// Required designer variable.
@@ -28,48 +28,289 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnDimension = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDimensionSettings));
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBaudRate = new System.Windows.Forms.TextBox();
+            this.txtBitLength = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtStopBits = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtParity = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPw = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // cmbPort
             // 
-            this.groupBox1.Controls.Add(this.btnDimension);
-            this.groupBox1.Location = new System.Drawing.Point(588, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(448, 223);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Settings";
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(101, 17);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(154, 21);
+            this.cmbPort.TabIndex = 1;
             // 
-            // btnDimension
+            // label4
             // 
-            this.btnDimension.Location = new System.Drawing.Point(6, 19);
-            this.btnDimension.Name = "btnDimension";
-            this.btnDimension.Size = new System.Drawing.Size(436, 23);
-            this.btnDimension.TabIndex = 0;
-            this.btnDimension.Text = "Dimension® Clinical Chemistry System Interface ";
-            this.btnDimension.UseVisualStyleBackColor = true;
-            this.btnDimension.Click += new System.EventHandler(this.btnDimension_Click);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Baud Rate";
             // 
-            // FormMain
+            // txtBaudRate
+            // 
+            this.txtBaudRate.Location = new System.Drawing.Point(101, 47);
+            this.txtBaudRate.Name = "txtBaudRate";
+            this.txtBaudRate.Size = new System.Drawing.Size(154, 20);
+            this.txtBaudRate.TabIndex = 6;
+            // 
+            // txtBitLength
+            // 
+            this.txtBitLength.Location = new System.Drawing.Point(101, 73);
+            this.txtBitLength.Name = "txtBitLength";
+            this.txtBitLength.Size = new System.Drawing.Size(154, 20);
+            this.txtBitLength.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Bit Length";
+            // 
+            // txtStopBits
+            // 
+            this.txtStopBits.Location = new System.Drawing.Point(101, 99);
+            this.txtStopBits.Name = "txtStopBits";
+            this.txtStopBits.Size = new System.Drawing.Size(154, 20);
+            this.txtStopBits.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Stop Bits";
+            // 
+            // txtParity
+            // 
+            this.txtParity.Location = new System.Drawing.Point(101, 125);
+            this.txtParity.Name = "txtParity";
+            this.txtParity.Size = new System.Drawing.Size(154, 20);
+            this.txtParity.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 125);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Parity";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(101, 151);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(154, 20);
+            this.txtUsername.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 151);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(101, 180);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(154, 20);
+            this.txtPassword.TabIndex = 17;
+            // 
+            // lblPw
+            // 
+            this.lblPw.AutoSize = true;
+            this.lblPw.Location = new System.Drawing.Point(12, 180);
+            this.lblPw.Name = "lblPw";
+            this.lblPw.Size = new System.Drawing.Size(53, 13);
+            this.lblPw.TabIndex = 16;
+            this.lblPw.Text = "Password";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(101, 206);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(327, 20);
+            this.txtUrl.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 206);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "LIMS Address";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(488, 7);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(97, 23);
+            this.btnClear.TabIndex = 26;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClearStatus_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(353, 232);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 28;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click_1);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(268, 232);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 27;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Port";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(434, 33);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txtStatus.Size = new System.Drawing.Size(591, 232);
+            this.txtStatus.TabIndex = 34;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(261, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(167, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 36;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(431, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "Status";
+            // 
+            // FormDimensionSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1048, 450);
-            this.Controls.Add(this.groupBox1);
-            this.Name = "FormMain";
-            this.Text = "Middleware";
-            this.groupBox1.ResumeLayout(false);
+            this.ClientSize = new System.Drawing.Size(1037, 276);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPw);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtParity);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtStopBits);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBitLength);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtBaudRate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbPort);
+            this.Name = "FormDimensionSettings";
+            this.Text = "SysMex Middleware";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormDimensionSettings_FormClosing);
+            this.Load += new System.EventHandler(this.FormDimensionSettings_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnDimension;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBaudRate;
+        private System.Windows.Forms.TextBox txtBitLength;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtStopBits;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtParity;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPw;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
-

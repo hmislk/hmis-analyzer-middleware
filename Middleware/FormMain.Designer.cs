aabfc744a7e14552cdb6110e51ee65cd576d06d9
Buddhika Ariyaratne <buddhika.ari@gmail.com>
2018-07-02 07:29:32 +0530

    Signed-off-by: Buddhika Ariyaratne <buddhika.ari@gmail.com>

--- Middleware/FormMain.Designer.cs
+++ Middleware/FormMain.Designer.cs
@@ -28,48 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.btnDimension = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPw = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.RichTextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnEnq = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // cmbPort
             // 
-            this.groupBox1.Controls.Add(this.btnDimension);
-            this.groupBox1.Location = new System.Drawing.Point(588, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(448, 223);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Settings";
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(101, 17);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(154, 21);
+            this.cmbPort.TabIndex = 1;
             // 
-            // btnDimension
+            // txtUsername
             // 
-            this.btnDimension.Location = new System.Drawing.Point(6, 19);
-            this.btnDimension.Name = "btnDimension";
-            this.btnDimension.Size = new System.Drawing.Size(436, 23);
-            this.btnDimension.TabIndex = 0;
-            this.btnDimension.Text = "Dimension® Clinical Chemistry System Interface ";
-            this.btnDimension.UseVisualStyleBackColor = true;
-            this.btnDimension.Click += new System.EventHandler(this.btnDimension_Click);
+            this.txtUsername.Location = new System.Drawing.Point(101, 68);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(154, 20);
+            this.txtUsername.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(101, 98);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(154, 20);
+            this.txtPassword.TabIndex = 17;
+            // 
+            // lblPw
+            // 
+            this.lblPw.AutoSize = true;
+            this.lblPw.Location = new System.Drawing.Point(14, 101);
+            this.lblPw.Name = "lblPw";
+            this.lblPw.Size = new System.Drawing.Size(53, 13);
+            this.lblPw.TabIndex = 16;
+            this.lblPw.Text = "Password";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(101, 42);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(446, 20);
+            this.txtUrl.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 42);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "LIMS Address";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(553, 122);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(97, 23);
+            this.btnClear.TabIndex = 26;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClearStatus_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(342, 15);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 28;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click_1);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(261, 15);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 27;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Port";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(15, 151);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(1038, 362);
+            this.txtStatus.TabIndex = 36;
+            this.txtStatus.Text = "";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(17, 122);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(530, 23);
+            this.progressBar1.TabIndex = 37;
+            // 
+            // btnEnq
+            // 
+            this.btnEnq.Location = new System.Drawing.Point(423, 15);
+            this.btnEnq.Name = "btnEnq";
+            this.btnEnq.Size = new System.Drawing.Size(75, 23);
+            this.btnEnq.TabIndex = 38;
+            this.btnEnq.Text = "ENQ";
+            this.btnEnq.UseVisualStyleBackColor = true;
+            this.btnEnq.Click += new System.EventHandler(this.btnEnq_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1048, 450);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1065, 525);
+            this.Controls.Add(this.btnEnq);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPw);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbPort);
             this.Name = "FormMain";
-            this.Text = "Middleware";
-            this.groupBox1.ResumeLayout(false);
+            this.Text = "Sysmex Middleware";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormDimensionSettings_FormClosing);
+            this.Load += new System.EventHandler(this.FormDimensionSettings_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnDimension;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPw;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnEnq;
     }
 }
-
--- Middleware/FormMain.cs
+++ Middleware/FormMain.cs
@@ -1,31 +1,501 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+using System.Web;
 
 namespace Middleware
 {
-    
-
-
     public partial class FormMain : Form
     {
-        private FormDimensionSettings dimensionsForm;
+
+        SerialPort com = new SerialPort();
+
+        private readonly HttpClient client = new HttpClient();
+        List<byte> temMessage = new List<byte>();
+        List<byte> message = new List<byte>();
+
+        string url = "";
+        string username = "";
+        string password = "";
+        string status = "";
+
+        int progressBarValue;
+
+        List<byte> messageInBytes = new List<byte>();
+
+        DateTime msgStartat;
+        Boolean arrayListBlocked;
+       
+        private void com_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytes = com.BytesToRead;
+            byte[] buffer = new byte[bytes];
+            com.Read(buffer, 0, bytes);
+
+            if (arrayListBlocked)
+            {
+                temMessage.AddRange(buffer);
+            }
+            else
+            {
+                arrayListBlocked = true;
+                temMessage.AddRange(buffer);
+                message.AddRange(temMessage);
+                temMessage = new List<byte>();
+                arrayListBlocked = false;
+            }
+           
+        }
+
+
+        private async Task sendDataToLimsAsync()
+        {
+            if (!arrayListBlocked)
+            {
+                arrayListBlocked = true;
+                Boolean arrayEmpty = !message.Any();
+                if(message!=null && !arrayEmpty)
+                {
+                    status += "Sending data LIMS" + Environment.NewLine;
+                    await SendDataToLimsAsync(message);
+                    this.Invoke(new EventHandler(DisplayText));
+                }
+                arrayListBlocked = false;
+            }
+            
+           
+            
+        }
+
+
+        private void com_DataReceivedWorking1(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytes = com.BytesToRead;
+            byte[] buffer = new byte[bytes];
+            com.Read(buffer, 0, bytes);
+            if (ContainEnd(buffer))
+            {
+                status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Message Received From Analyzer." + Environment.NewLine;
+                temMessage.AddRange(buffer);
+                message.AddRange(temMessage);
+                String ts = "";
+                foreach (byte b in temMessage)
+                {
+                    ts += b + " ";
+                }
+                status += ts + Environment.NewLine;
+                SendDataToLimsAsync(temMessage);
+                temMessage = new List<byte>();
+                status += "End of a Message " + Environment.NewLine;
+            }
+            else
+            {
+                temMessage.AddRange(buffer);
+                String ts = "";
+                foreach (byte b in buffer)
+                {
+                    ts += b + "|";
+                }
+                status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Part of a Message " + ts + Environment.NewLine;
+            }
+
+            this.Invoke(new EventHandler(DisplayText));
+
+        }
+
+
+        private void com_DataReceivedOld(object sender, SerialDataReceivedEventArgs e)
+        {
+            int bytes = com.BytesToRead;
+            byte[] buffer = new byte[bytes];
+            com.Read(buffer, 0, bytes);
+            if (bytes == 1)
+            {
+                String temAscii = System.Text.Encoding.ASCII.GetString(new[] { buffer[0] });
+                if (buffer[0] == byteEnq())
+                {
+                    status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " ENQ Received" + Environment.NewLine;
+                    com.Write(ASCII.ACK.ToString());
+                    status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " ACK Sent" + Environment.NewLine;
+                }
+                if (buffer[0] == byteAck())
+                {
+                    status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Ack Received" + Environment.NewLine;
+                }
+            }
+            else
+            {
+                if (ContainEnd(buffer))
+                {
+                    status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Message Received From Analyzer." + Environment.NewLine;
+                    temMessage.AddRange(buffer);
+                    message.AddRange(temMessage);
+                    status += bytesToString(temMessage) + Environment.NewLine;
+                    SendDataToLimsAsync(temMessage);
+                    temMessage = new List<byte>();
+                    status += "End of a Message " + Environment.NewLine;
+                    com.Write(Ack());
+                    status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " ACK sent to Analyzer." + Environment.NewLine;
+                }
+                else
+                {
+                    temMessage.AddRange(buffer);
+                    String ts = "";
+                    foreach (byte b in buffer)
+                    {
+                        ts += b + "|";
+                    }
+                    status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Part of a Message " + ts + Environment.NewLine;
+                }
+            }
+            this.Invoke(new EventHandler(DisplayText));
+
+        }
+
+        public byte byteEnq()
+        {
+            return 5;
+        }
+
+        public byte byteStx()
+        {
+            return 2;
+        }
+
+        public byte byteAck()
+        {
+            return 6;
+        }
+
+        public byte byteEtx()
+        {
+            return 3;
+        }
+
+        public byte byteEot()
+        {
+            return 4;
+        }
+
+        public byte byteEob()
+        {
+            return 17;
+        }
+
+        public byte byteNak()
+        {
+            return 15;
+        }
+
+
+        private Boolean ContainEnd(Byte[] bytesToCheck)
+        {
+            foreach (Byte b in bytesToCheck)
+            {
+                if (b == byteEtx() || b == byteEot())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+        private async Task SendDataToLimsAsync(List<byte> bytes)
+        {
+            String sendingStr = "";
+            foreach (byte b in bytes)
+            {
+                sendingStr += b + " ";
+            }
+
+            string longurl = url;
+            var uriBuilder = new UriBuilder(longurl);
+
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            query["username"] = txtUsername.Text;
+            query["password"] = txtPassword.Text;
+            query["machine"] = "SysMex";
+            query["msg"] = sendingStr;
+
+            status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Sending Data to LIMS" + Environment.NewLine;
+
+            uriBuilder.Query = query.ToString();
+            longurl = uriBuilder.ToString();
+
+            status += longurl + Environment.NewLine;
+
+            // var values = new Dictionary<string, string> { { "username", username }, { "password", password }, { "msg", msg } };
+            // var content = new FormUrlEncodedContent(values);
+
+            var response = await client.GetAsync(longurl);
+            var responseString = await response.Content.ReadAsStringAsync();
+            responseString = ExtractMessageFromHtml(responseString);
+            status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Received Data From LIMS" + Environment.NewLine;
+            status += responseString + Environment.NewLine;
+            com.Write(Stx() + responseString + Etx());
+            status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Sending Data to Analyzer" + Environment.NewLine;
+            this.Invoke(new EventHandler(DisplayText));
+
+        }
+
+
+        #region functions
+
+
+
+        private String Cr()
+        {
+            return Character(13);
+        }
+
+
+        private String Etb()
+        {
+            return Character(23);
+        }
+
+        private String Lf()
+        {
+            return Character(10);
+        }
+
+        private String Ack()
+        {
+            return Character(6);
+        }
+
+        private String Nak()
+        {
+            return Character(21);
+        }
+
+        private String Stx()
+        {
+            return Character(2);
+        }
+
+        private String Etx()
+        {
+            return Character(3);
+        }
+
+        private String Enq()
+        {
+            return Character(5);
+        }
+
+        private String Character(int charNo)
+        {
+            char ack = (char)charNo;
+            String m = ack.ToString();
+            return m;
+        }
+
+
+        private byte[] StringsToBytes(String value)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes(value);
+            return bytes;
+        }
+
+
+
+
+        private String ExtractMessageFromHtml(String html)
+        {
+            String s = html;
+            int pFrom = s.IndexOf("#{") + "#{".Length;
+            int pTo = s.LastIndexOf("}");
+            String result = s.Substring(pFrom, pTo - pFrom);
+            return result;
+        }
+
+        private String bytesToString(List<byte> bytesToConvert)
+        {
+            Byte[] temBytes = bytesToConvert.ToArray();
+            String temStr = "";
+            temStr = Encoding.ASCII.GetString(temBytes, 0, temBytes.Length);
+            return temStr;
+        }
+
+        #endregion
+
+
+        #region FormEvents
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            DisplayText();
+        }
+
+        private void DisplayText()
+        {
+            txtStatus.Text = status;
+            if (progressBarValue < 100)
+            {
+                progressBarValue++;
+            }
+            else
+            {
+                progressBarValue = 0;
+            }
+            progressBar1.Value = progressBarValue;
+            progressBar1.Update();
+        }
 
         public FormMain()
         {
             InitializeComponent();
         }
 
-        private void btnDimension_Click(object sender, EventArgs e)
+        private void FormDimensionSettings_Load(object sender, EventArgs e)
         {
-            dimensionsForm = new FormDimensionSettings();
-            dimensionsForm.Show();
+            String[] ports = SerialPort.GetPortNames();
+            cmbPort.Items.AddRange(ports);
+            cmbPort.SelectedIndex = 0;
+            btnClose.Enabled = false;
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true);
+
+            key.CreateSubKey("Middleware");
+            key = key.OpenSubKey("Middleware", true);
+
+
+            key.CreateSubKey("SysMex");
+            key = key.OpenSubKey("SysMex", true);
+
+            progressBarValue = 0;
+
+            if (key == null)
+            {
+                cmbPort.Text = "";
+            }
+            else
+            {
+                cmbPort.Text = (String)key.GetValue("Port");
+                txtUrl.Text = (String)key.GetValue("Url", "");
+            }
+
+
+
+
+        }
+
+
+        private void FormDimensionSettings_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Software", true);
+
+            key.CreateSubKey("Middleware");
+            key = key.OpenSubKey("Middleware", true);
+
+
+            key.CreateSubKey("SysMex");
+            key = key.OpenSubKey("SysMex", true);
+
+            key.SetValue("Port", cmbPort.Text);
+            key.SetValue("Url", txtUrl.Text);
+
+        }
+
+        private void btnListStatus_Click(object sender, EventArgs e)
+        {
+            DisplayText();
+        }
+
+        private void btnClearStatus_Click(object sender, EventArgs e)
+        {
+            message = new List<Byte>();
+            temMessage = new List<Byte>();
+            status = "";
+            DisplayText();
+        }
+
+        private void btnClose_Click_1(object sender, EventArgs e)
+        {
+            btnOpen.Enabled = true;
+            btnClose.Enabled = false;
+
+            try
+            {
+                com.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        #endregion
+
+
+
+        private void btnClearMessages_Click(object sender, EventArgs e)
+        {
+
+            status = "";
+            DisplayText();
+        }
+
+        private void btnOpen_Click_1(object sender, EventArgs e)
+        {
+            msgStartat = DateTime.Now;
+            btnOpen.Enabled = false;
+            btnClose.Enabled = true;
+            url = txtUrl.Text;
+            username = txtUsername.Text;
+            password = txtPassword.Text;
+            try
+            {
+                com.PortName = cmbPort.Text;
+                com.BaudRate = 9600;
+                com.DataBits = 8;
+                com.ReadBufferSize = 100000;
+                com.StopBits = StopBits.One;
+                com.Parity = Parity.None;
+                com.DtrEnable = true;
+                com.RtsEnable = true;
+                com.Open();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            com.DataReceived += new SerialDataReceivedEventHandler(com_DataReceived);
+
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(1);
+
+            var timer = new System.Threading.Timer((ex) =>
+            {
+                sendDataToLimsAsync();
+            }, null, startTimeSpan, periodTimeSpan);
+        }
+
+
+        private void btnEnq_Click(object sender, EventArgs e)
+        {
+            if (com.IsOpen)
+            {
+                com.Write(Enq());
+                status += DateTime.Now.ToString("dd MMM yyyy hh:mm:ss tt") + " Sending Enq to Analyzer" + Environment.NewLine;
+                this.Invoke(new EventHandler(DisplayText));
+            }
         }
     }
 }
